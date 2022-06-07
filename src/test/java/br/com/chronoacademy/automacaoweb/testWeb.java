1764266ccd36991df5b4d48aa416be555345b09f
Your Name <keziavilamoreira@gmail.comgit confirg --global user.name Kezia>
2022-06-07 18:22:01 -0300

    Anotacoes 06

--- src/test/java/br/com/chronoacademy/automacaoweb/testWeb.java
+++ src/test/java/br/com/chronoacademy/automacaoweb/testWeb.java
@@ -1,6 +1,8 @@
 package br.com.chronoacademy.automacaoweb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,19 +12,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertEquals;
 
 public class testWeb {
-    @Test
-    public void primeiroTest(){
+    WebDriver driver;
+
+    @Before
+    public void inicializaTeste() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver;
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.chronosacademy.com.br");
+
+    }
+
+    @Test
+    public void primeiroTest() {
         String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
         assertEquals("Porque Tempo É Conhecimento", titulo);
+
+    }
+    @After
+    public void finalizaTest(){
         driver.quit();
-
-
     }
 }
